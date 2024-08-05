b97e45410facd1bc48e42ff28853a1fc2d241221
katlegorphele <katlegorphele@gmail.com>
2024-08-05 19:34:18 +0200

    Updated hardhat project setup

--- packages/hardhat/ignition/modules/Lock.ts
+++ packages/hardhat/ignition/modules/Lock.ts
@@ -1,9 +1,9 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
+const ONE_GWEI: bigint = 1_000_000_000n;
 
-module.exports = buildModule("LockModule", (m) => {
+const LockModule = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
@@ -13,3 +13,5 @@ module.exports = buildModule("LockModule", (m) => {
 
   return { lock };
 });
+
+export default LockModule;
