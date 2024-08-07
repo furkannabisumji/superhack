56490e6fb72bd00ba3d7c91a091dae3addef19a3
henapartae123 <henyvibe@gmail.com>
2024-08-07 16:58:22 +0300

    Created DAO UI

--- packages/react-app/pages/my-profile/index.tsx
+++ packages/react-app/pages/my-profile/index.tsx
@@ -1,6 +1,11 @@
 import ProductCard from "@/components/ProductCard";
+import Chip from "@mui/material/Chip";
+import { Stack } from "@mui/material";
 import Link from "next/link";
 import { SetStateAction, useEffect, useState } from "react";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
 
 const MyProfile = () => {
   const [activeIndex, setactiveIndex] = useState(1);
@@ -333,17 +338,145 @@ const MyProfile = () => {
           <div>{/* messages */}</div>
         </div>
       </div>
-      <div className={`pageContent ${checkActive(4, "active")}`}>
+      <div className={`pageContent ${checkActive(5, "active")}`}>
         <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-2/3 overflow-hidden -mt-10">
-          <div className="h-[140px] bg-gradient-to-r from-slate-400 to-slate-500">
-            <div className="flex flex-row px-10 py-20 justify-between">
+          <div className="h-screen bg-gradient-to-r from-slate-400 to-slate-500">
+            <div className="flex flex-col gap-10 px-10 py-10 justify-between">
               <h1 className="text-3xl">Our Governance</h1>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea a
-                hic quia! Libero voluptas neque iusto corrupti, sequi nobis
-                numquam quaerat deleniti, modi voluptate expedita eligendi
-                aperiam reprehenderit dolor vero.
+              <p className="text-sm">
+                Following the request for proposal (RFP) from NEW (Network
+                Expansion WorkGroup) that concluded on Decemeber 5th, 2023 there
+                are 4 eligible submissions to developer wstEth brige on BNB
+                recieved postivie or neutral feedback from the community. This
+                vote will identify the most supported team among the eligible
+                competing submissions whose references given below to kick off
+                or continue development.
+                <div className="flex flex-col gap-2">
+                  <h2>Voting conditions</h2>
+                  <p>
+                    This vote will requires a quoroum of 5% LDO total supply
+                    (50M) votes for any single option . The winning submission
+                    will be determined based on two conditions.
+                  </p>
+                  <ul>
+                    <li>
+                      1. a quoroum of 50% in total accross all options is
+                      reached.
+                    </li>
+                    <li>
+                      2. The leading option must exceed the second candidate by
+                      a margin of 20% of votes participated in the vote
+                    </li>
+                  </ul>
+                </div>
               </p>
+
+              <div className="flex flex-col break-inside py-2 px-5 rounded-xl bg-white dark:bg-slate-800 bg-clip-border">
+                <div className="flex flex-row justify-between items-center">
+                  {/* left */}
+
+                  <div className="flex flex-col gap-2">
+                    <h2 className="p-3">Proposal 1</h2>
+
+                    <Chip
+                      label="Passed"
+                      variant="outlined"
+                      size="small"
+                      className="text-[11px] p-2 text-green-400 border-green-400"
+                    />
+                  </div>
+
+                  {/* right */}
+                  <div className="flex flex-col gap-2">
+                    <div className="flex flex-row gap-2">
+                      <Link
+                        href="/"
+                        className="rounded-full border border-green-bg-green-400 bg-green-500 py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-xs font-inter flex items-center justify-center"
+                      >
+                        Let's do it
+                      </Link>
+                      <div className="flex flex-col">
+                        <h2>50%</h2>
+                        <LinearProgress
+                          color="success"
+                          variant="determinate"
+                          value={50}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex flex-row gap-2">
+                      <Link
+                        href="/"
+                        className="rounded-full border border-red-500 bg-red-border-red-500 py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-xs font-inter flex items-center justify-center"
+                      >
+                        No way
+                      </Link>
+                      <div className="flex flex-col">
+                        <h2>50%</h2>
+                        <LinearProgress
+                          color="error"
+                          variant="determinate"
+                          value={50}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col break-inside py-2 px-5 rounded-xl bg-white dark:bg-slate-800 bg-clip-border">
+                <div className="flex flex-row justify-between items-center">
+                  {/* left */}
+
+                  <div className="flex flex-col gap-2">
+                    <h2 className="p-3">Proposal 2</h2>
+
+                    <Chip
+                      label="Passed"
+                      variant="outlined"
+                      size="small"
+                      className="text-[11px] p-2 text-green-400 border-green-400"
+                    />
+                  </div>
+
+                  {/* right */}
+                  <div className="flex flex-col gap-2">
+                    <div className="flex flex-row gap-2">
+                      <Link
+                        href="/"
+                        className="rounded-full border border-green-bg-green-400 bg-green-500 py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-xs font-inter flex items-center justify-center"
+                      >
+                        Let's do it
+                      </Link>
+                      <div className="flex flex-col">
+                        <h2>50%</h2>
+                        <LinearProgress
+                          color="success"
+                          variant="determinate"
+                          value={50}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex flex-row gap-2">
+                      <Link
+                        href="/"
+                        className="rounded-full border border-red-500 bg-red-border-red-500 py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-xs font-inter flex items-center justify-center"
+                      >
+                        No way
+                      </Link>
+                      <div className="flex flex-col">
+                        <h2>50%</h2>
+                        <LinearProgress
+                          color="error"
+                          variant="determinate"
+                          value={50}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div>{/* messages */}</div>
