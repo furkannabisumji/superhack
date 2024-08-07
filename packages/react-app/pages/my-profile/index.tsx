ee74c4ff7256a1be8df072eeae3c7203e39afff7
henapartae123 <henyvibe@gmail.com>
2024-08-07 14:42:52 +0300

    fixed bug

--- packages/react-app/pages/my-profile/index.tsx
+++ packages/react-app/pages/my-profile/index.tsx
@@ -125,6 +125,35 @@ const MyProfile = () => {
                 </span>
               </button>
             </li>
+            <li>
+              <button
+                type="button"
+                onClick={() => handleClick(5)}
+                className={`nav ${checkActive(
+                  5,
+                  "active"
+                )} flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700`}
+                aria-controls="dropdown-sales"
+                data-collapse-toggle="dropdown-sales"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span className="flex-1 ml-3 text-left whitespace-nowrap">
+                  DAO
+                </span>
+              </button>
+            </li>
           </ul>
           <ul className="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
             <li>
@@ -299,8 +328,22 @@ const MyProfile = () => {
           <div className="h-[140px] bg-gradient-to-r from-slate-400 to-slate-500">
             <div className="flex flex-row px-10 py-20 justify-between">
               <h1 className="text-3xl">Messages</h1>
-              {/* will show a pop up of available wallets */}
-              {/* base or minipay */}
+            </div>
+          </div>
+          <div>{/* messages */}</div>
+        </div>
+      </div>
+      <div className={`pageContent ${checkActive(4, "active")}`}>
+        <div className="bg-white md:mx-auto rounded shadow-xl w-full md:w-2/3 overflow-hidden -mt-10">
+          <div className="h-[140px] bg-gradient-to-r from-slate-400 to-slate-500">
+            <div className="flex flex-row px-10 py-20 justify-between">
+              <h1 className="text-3xl">Our Governance</h1>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea a
+                hic quia! Libero voluptas neque iusto corrupti, sequi nobis
+                numquam quaerat deleniti, modi voluptate expedita eligendi
+                aperiam reprehenderit dolor vero.
+              </p>
             </div>
           </div>
           <div>{/* messages */}</div>
