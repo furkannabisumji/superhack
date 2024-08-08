009fbcc95ccb4b1d7b1f46ff74fdbc8bd5ee9edf
Habte Selassie <habtuchelsea1000@gmail.com>
2024-08-08 12:09:18 +0300

    update

--- packages/hardhat/Pyth-Integration/off-chain.ts
+++ packages/hardhat/Pyth-Integration/off-chain.ts
@@ -10,6 +10,8 @@ async function priceFetcher() {
     }
   });
 
+  
+
   // Define the price IDs you are interested in
   const priceIds = [
     "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43", // BTC/USD price id
